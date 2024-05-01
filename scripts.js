ee49bbfb01e86a251607abe99bcd9a20bd9ef4c1
KevinsitoHB <family-hernandez2014@outlook.com>
2024-05-01 16:19:05 -0400

    Promise

--- scripts.js
+++ scripts.js
@@ -29,4 +29,45 @@ async function getEmployees() {
 function buttonLoadClick() {
   console.log('Button clicked');
 }
-getEmployees();
+// getEmployees();
+
+//PROMISES
+function myPromise() {
+  return new Promise((resolved, denied) => {
+    setTimeout(() => {
+      // resolved({
+      //   name: 'Kevin',
+      //   last: 'Hernandez',
+      // }),
+      denied({
+        codigo: '0',
+        message: 'Error',
+      });
+    }, 3000);
+  });
+}
+const result = myPromise();
+console.log(result);
+
+// result
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .then(() => {
+//     console.log('Otra cosa');
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+async function functionAsync() {
+  try {
+    const result = await myPromise();
+    console.log('Trying');
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('Execute');
+  }
+}
+functionAsync();
